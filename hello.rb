ffa5b2ed72422ccbd677bf7f0a2a9d16a4b029e6
Lutemia <Lutemia@users.noreply.github.com>
2017-12-29 19:23:24 -0700

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,16 @@
+puts "What is your name? ^_^"
+name = gets.chomp
+puts "hi #{name} :D"
+puts "How old are you? :o"
+age = gets.chomp
+puts "I see, you are #{age} years old! ^_^"
+puts "What do you do for a living?"
+job = gets.chomp
+puts "I see, your job is #{job} :o"
+puts "What is the meaning of life?"
+life = gets.chomp
+if life == 42
+  puts "Very cheeky! ^_^"
+else
+  puts "So you do not know either. It's okay. I will keep looking for someone who does. . ."
+end
